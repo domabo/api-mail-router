f0918fa73d11c02c1612f4db4a4d11209f4d0ecb
Domabo <admin@domabo.com>
2014-06-22 09:04:06 -0500

    refactored podio contact

--- api-mail-post-podio.php
+++ api-mail-post-podio.php
@@ -1,49 +1,170 @@
 <?php
+
 require_once 'Podio/PodioAPI.php';
 
 function sendPodio($data)
 {
-$PODIO_CLIENTID = get_option('PODIO_CLIENTID');
-$PODIO_CLIENTSECRET = get_option('PODIO_CLIENTSECRET');
-$PODIO_APPID = get_option('PODIO_APPID1');
-$PODIO_APPSECRET = get_option('PODIO_APPSECRET1');
-$PODIO_SPACEID = get_option('PODIO_SPACEID');
+  $PODIO_CLIENTID = get_option('PODIO_CLIENTID');
+  $PODIO_CLIENTSECRET = get_option('PODIO_CLIENTSECRET');
+  $PODIO_APPID = get_option('PODIO_APPID1');
+  $PODIO_APPSECRET = get_option('PODIO_APPSECRET1');
+  $PODIO_SPACEID = get_option('PODIO_SPACEID');
 
-try
-{
+  $contact_name = $data['merges']['FNAME'] . ' ' . $data['merges']['LNAME'];
+  $contact_email = $data['email'];
 
-Podio::setup($PODIO_CLIENTID, $PODIO_CLIENTSECRET);
-Podio::authenticate_with_app($PODIO_APPID, $PODIO_APPSECRET);
+  $item_fields =  array(
+    "source" => 1 );
 
-$contact_fields = array(
-    "name"=>($data['merges']['FNAME'] . ' ' . $data['merges']['LNAME']),
-    "mail"=>array($data['email'])
-    );
+  if (!empty($data['merges']['FB_ID']))
+  {
+    $contact_facebook = $data['merges']['FB_ID'];
 
-$ep_profile_id = PodioContact::create( $PODIO_SPACEID, $contact_fields);
+    $item_fields["facebook-id"] = $contact_facebook;
+  }
+  else
+  {
+    $contact_facebook = "";
+  }
 
+  amrPodio::authenticate($PODIO_CLIENTID, $PODIO_CLIENTSECRET, $PODIO_APPID, $PODIO_APPSECRET);
 
-if (!empty($data['merges']['FB_ID']))
-{
-$item_fields =  array(
-          "contact" => $ep_profile_id,
-          "facebook-id" => $data['merges']['FB_ID'],
-           "source" => 1 );
-} else
-{
-    $item_fields =  array(
-          "contact" => $ep_profile_id,
-           "source" => 1 );
-}
-           
-PodioItem::create( $PODIO_APPID,  array('fields' => $item_fields));
-
-} catch (PodioError $e) {
-  echo 'Caught Pod exception: ' . $e->body['error_description'] ;
-
-} catch (Exception $e) {
-    echo 'Caught exception: ' .  $e->getMessage(), "\n";
-}
+  return amrPodio::createContactItem($PODIO_APPID, $PODIO_SPACEID, $contact_name, $contact_email, $contact_facebook, $item_fields, "contact");
 
 }
+
+class amrFacebook
+{
+  public static function get_fb_img($fbId)
+  {
+    try
+    {
+      $url = 'http://graph.facebook.com/' . $fbId . '/picture?type=large';
+      $headers = get_headers($url,1);
+
+      $profileimage = $headers['Location']; 
+
+      $ext = pathinfo($profileimage, PATHINFO_EXTENSION);
+      $filename = sys_get_temp_dir() . "/" . $fbId . "." . $ext;
+
+      if (file_exists($filename)) 
+      {
+        return $filename;
+      } else 
+      {
+
+        $ch = curl_init($profileimage);
+        $fp = fopen( $filename, "wb");
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_USERAGENT,"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, wie z. B. Gecko) Chrome/13.0.782.215 Safari/525.13." );
+        curl_exec($ch);
+        curl_close($ch);
+        fclose($fp);
+        return $filename;
+      }
+    }  catch (Exception $e) {
+      return null;
+    }
+  }
+}
+
+class amrPodio
+{
+
+  public static function authenticate($PODIO_CLIENTID, $PODIO_CLIENTSECRET, $PODIO_APPID, $PODIO_APPTOKEN)
+  {
+    Podio::setup($PODIO_CLIENTID, $PODIO_CLIENTSECRET);
+    Podio::authenticate_with_app($PODIO_APPID, $PODIO_APPTOKEN);
+  }
+
+  public static function createContactItem($appid, $spaceid, $contact_name, $contact_email, $contact_facebook, &$item_fields, $contact_target_tag)
+  {
+    try
+    {
+      $contact_fields_index = array("name"=>$contact_name, "mail"=>array($contact_email));
+      $contact_fields = $contact_fields_index;
+
+
+      if (!empty($contact_facebook))
+      {
+        $filename = amrFacebook::get_fb_img($contact_facebook);
+        if ($filename)
+        {
+          $fid = PodioFile::upload ($filename, $contact_facebook . ".jpg");
+          $contact_fields["avatar"] = ($fid->file_id);
+        }
+      }
+
+      $existingContacts = PodioContact::get_for_app( $appid, $attributes = $contact_fields_index);
+
+      if (count($existingContacts)>0)
+      {
+        $first =  $existingContacts[0];
+        $ep_profile_id = $first->profile_id;
+
+        PodioContact::update( $ep_profile_id, $contact_fields );
+
+      } else
+      {
+        $ep_profile_id = PodioContact::create( $spaceid, $contact_fields);
+      }
+
+      $item_fields[$contact_target_tag] = $ep_profile_id;
+
+      PodioItem::create( $appid,  array('fields' => $item_fields));
+      return null;
+
+    } catch (PodioError $e) {
+
+      return self::createErrorTask($appid, 
+        $spaceid, 
+        $contact_name, 
+        "There was an error. Podio responded with the error type " . $e->body['error'] ." and the mesage " . $e->body['error_description'] . "."
+        );
+
+    } catch (Exception $e) {
+
+      return self::createErrorTask($appid, 
+        $spaceid, 
+        $contact_name, 
+        "There was a general exception: " . $$e->getMessage()
+        );
+    }
+  }
+
+  public static function createErrorTask($appid, $spaceid, $contact_name, $description)
+  {
+    $title = "API Error creating Podio Item";
+
+    if (!empty($contact_name))
+      $title = $title . " for " . $contact_name;
+
+    $err = self::createTask($appid, $spaceid, $title, $description);
+
+    if ($err)
+      $description = $description . "     " . $err;
+
+    return $description;
+  }
+
+  public static function createTask($appid, $spaceid, $title, $description)
+  {
+    try  
+    {
+      $task = PodioTask::create_for( "app", $appid, $attributes = array( "text" => $title,
+        "private" => false,
+        "description" => $description,
+        "status" => "active",
+        "space_id" => $spaceid), $options = array() );
+
+      return null;
+    }  
+    catch (PodioError $te) 
+    {
+      return "There was an error. The API responded with the error type " . $te->body['error'] ." and the mesage " . $te->body['error_description'] . ".";
+    }
+  }
+}
+
 ?>
--- api-mail-router-mailchimp-post.php
+++ api-mail-router-mailchimp-post.php
@@ -18,50 +18,52 @@ require_once( 'api-mail-post-podio.php' );
 $MAILCHIMP_WEBHOOK_KEY = get_option('MAILCHIMP_WEBHOOK_KEY');
 
 if ( !isset($_GET['key']) ){
-   //  No security key specified, ignoring request
+//  No security key specified, ignoring request
 } elseif ($_GET['key'] != $MAILCHIMP_WEBHOOK_KEY) {
-   //  Security key specified, but not correct
+//  Security key specified, but not correct
 } else {
-    //process the request
-    switch($_POST['type']){
-        case 'subscribe'  : subscribe($_POST['data']);   break;
-        case 'unsubscribe': unsubscribe($_POST['data']); break;
-        case 'cleaned'    : cleaned($_POST['data']);     break;
-        case 'upemail'    : upemail($_POST['data']);     break;
-        case 'profile'    : profile($_POST['data']);     break;
-        default:
-          //  Request type unknown, ignoring
-    }
+//process the request
+  switch($_POST['type']){
+    case 'subscribe'  : subscribe($_POST['data']);   break;
+    case 'unsubscribe': unsubscribe($_POST['data']); break;
+    case 'cleaned'    : cleaned($_POST['data']);     break;
+    case 'upemail'    : upemail($_POST['data']);     break;
+    case 'profile'    : profile($_POST['data']);     break;
+    default:
+//  Request type unknown, ignoring
+  }
 }
 
 /***********************************************
-    Helper Functions
+Helper Functions
 ***********************************************/
 
 function subscribe($data){
-    // $data['email'] just subscribed
-   echo 'RECEIVED SUBSCRIBE';
-    sendMandrill($data);
-    echo 'SENT WELCOME EMAIL';
-    sendPodio($data);
-     echo 'PROCESSED SUBSCRIBE';
-   
+// $data['email'] just subscribed
+  echo 'RECEIVED SUBSCRIBE';
+  sendMandrill($data);
+  echo 'SENT WELCOME EMAIL';
+  $retval = sendPodio($data);
+  if (empty($retval))
+    echo 'PROCESSED SUBSCRIBE';
+  else
+    echo $retval;
 }
 function unsubscribe($data){
-   // $data['email'] just unsubscribed
-    
+// $data['email'] just unsubscribed
+
 }
 function cleaned($data){
-   // $data['email']  was cleaned from  list
-    
+// $data['email']  was cleaned from  list
+
 }
 function upemail($data){
-    // $data['old_email'] changed their email address to  $data['new_email']
-    
+// $data['old_email'] changed their email address to  $data['new_email']
+
 }
 function profile($data){
-   sendMandrill($data);
-    // $data['email']  updated their profile
+  sendMandrill($data);
+// $data['email']  updated their profile
 }
 
 
