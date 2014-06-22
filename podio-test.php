d27c0daf882c0a142ae54e826025e9496f070486
Domabo <admin@domabo.com>
2014-06-22 09:34:46 -0500

    updated podio

--- podio-test.php
+++ podio-test.php
@@ -2,29 +2,25 @@
 
 try {
 
-echo "STARTING10<br>";
+
  define( 'WP_USE_THEMES', false );
 # Load WordPress Core
 // Assuming we're in a subdir: "~/wp-content/plugins/current_dir"
 require_once( '../../../wp-load.php' );
-
-echo "STARTING3<br>";
 require_once 'Podio/PodioAPI.php';
-echo "STARTING2<br>";
-
   $PODIO_CLIENTID = get_option('PODIO_CLIENTID');
   $PODIO_CLIENTSECRET = get_option('PODIO_CLIENTSECRET');
   $PODIO_APPID = get_option('PODIO_APPID1');
   $PODIO_APPSECRET = get_option('PODIO_APPSECRET1');
   $PODIO_SPACEID = get_option('PODIO_SPACEID');
 
-
  Podio::setup($PODIO_CLIENTID, $PODIO_CLIENTSECRET);
     Podio::authenticate_with_app($PODIO_APPID, $PODIO_APPSECRET);
 
- $contact_fields_index = array("mail"=>array("guy-facebook@barnardmail.net"));
-
-   $existingContacts = PodioContact::get_for_app( $appid, $attributes = $contact_fields_index);
+ $contact_fields_index = array("name"=>"Guy Barnard");
+echo "STARTING<br>";
+   $existingContacts = PodioContact::get_for_app( $PODIO_APPID , $attributes = $contact_fields_index);
+echo "DONE<br>";
 
    print_r($existingContacts);
 echo "<br>DONE<br";
