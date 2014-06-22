821fe864d390768bf6c027da60c49b85d06c0ce6
Domabo <admin@domabo.com>
2014-06-22 09:21:14 -0500

    updated podio

--- podio-test.php
+++ podio-test.php
@@ -1,4 +1,16 @@
 <?php
+
+ @ini_set( 'log_errors', 'Off' );
+
+@ini_set( 'display_errors', 'On' );
+
+@ini_set( 'error_reporting', E_ALL );
+
+define( 'WP_DEBUG', true );
+
+define( 'WP_DEBUG_LOG', false );
+
+define( 'WP_DEBUG_DISPLAY', true );
 try {
 echo "STARTING<br";
 define( 'WP_USE_THEMES', false );
